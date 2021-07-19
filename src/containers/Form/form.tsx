382af6f87e7a8c02d079763818b38699a577ae88
elizabethlumban <lumban@sg.ibm.com>
2021-07-19 08:41:29 +0800

    add reset button,simple validation check

--- src/containers/Form/form.tsx
+++ src/containers/Form/form.tsx
@@ -1,25 +1,31 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import { Formik, Form } from "formik";
 import { useSelector, useDispatch } from "react-redux";
 import { insertRecord } from "actions/items";
 import { IState } from "store/types";
+import { TextField, Button } from "@material-ui/core";
 
 export const Basic = () => {
   const dispatch = useDispatch();
   const items = useSelector((state: IState) => state.items);
   const loading = useSelector((state: IState) => state.loading);
 
+  const validate = (values: any) => {
+    const errors = {} as any;
+    //TODO: add more validation check for URL
+    if (!values.url) {
+      errors.url = "Please provide URL";
+    }
+    return errors;
+  };
   return (
     <div style={{ height: 10, margin: 80 }}>
-      {/*      <h2>Provide URL</h2> */}
       <Formik
         initialValues={{
-          firstName: "",
-          lastName: "",
+          url: "",
         }}
+        validate={validate}
         onSubmit={async (values: any) => {
-          /* await new Promise((r) => setTimeout(r, 500)); */
-          /* alert(JSON.stringify(values, null, 2)); */
           const update: any = {
             fullUrl: values.url,
           };
@@ -28,13 +34,27 @@ export const Basic = () => {
           } catch (error) {}
         }}
       >
-        <Form>
-          <label htmlFor="url">URL: </label>
-          <Field style={{ width: 900 }} id="url" name="url" placeholder="http://google.com/1344" />
-          <br></br>
-          <br></br>
-          <button type="submit">Submit</button>
-        </Form>
+        {({ values, errors, handleChange, handleSubmit, /*isSubmitting, setFieldValue,*/ dirty, isValid }) => (
+          <Form onSubmit={handleSubmit}>
+            <TextField
+              style={{ width: 900 }}
+              variant="outlined"
+              name="url"
+              id="url"
+              label="Url"
+              onChange={handleChange}
+              value={values.url}
+              error={Boolean(errors.url)}
+            />
+            <div>{Boolean(errors.url) ? errors.url : ""}</div>
+            <Button type="submit" variant="contained" color="primary" disabled={!(dirty && isValid)}>
+              Submit
+            </Button>
+            <Button type="reset" variant="contained" color="secondary">
+              Reset
+            </Button>
+          </Form>
+        )}
       </Formik>
       <div>
         {" "}
